2eed712cea78d6c43b801c39f317d22d49137b15
Pardim93 <wellingtons_10@hotmail.com>
2019-01-22 00:30:23 -0800

    add selenium test

--- auto_login.py
+++ auto_login.py
@@ -1,11 +1,34 @@
-import mechanize, re
+# import mechanize, re, urllib
+# br = mechanize.Browser()
+# # br.open('http://localhost:3000/articles')
+# br.open('http://localhost:3000/articles')
+# # agent = mechanize.new
+#
+# params = {'name': 'dhh', 'password':'secret'}
+# data = urllib.parse.urlencode(params)
+# # request  = mechanize.Request('http://localhost:3000/articles/new')
+# # response = mechanize.urlopen(request,data)
+# # print(dir(response.read()))
+# # payload = {'username': 'dhh', 'password': 'secret'}
+# br.find_link(text='New article')
+# # r = mechanize.Request(url='http://localhost:3000/articles', data=payload, headers=payload)
+# # print(r.visit)
+# # print (dir(r))
+#
+# # print (r.data())
+# req = br.click_link(text='New article')
+# req.set_data(data)
+# print(br.open(req))
+#
+# #print (br.response().read())
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-br = mechanize.Browser()
-br.open("localhost:3000")
-response1 = br.follow_link(text_regex=r"cheese\s*shop", nr=1)
-print(br.title())
-print(response1.geturl())
-print(response1.info())  # headers
-print(response1.read())  # body
+browser = webdriver.Chrome('C:\webbrowser\chromedriver.exe')
+browser.get('localhost:3000/articles')
+browser.find_element_by_link_text('New article').click()
+print(dir(browser))
 
-#br.select_form(name="order")
+# browser.send_key("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+# browser.send_key("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
