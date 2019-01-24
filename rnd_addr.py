34109d89df6965132e2f88c4a0c564ba68515e0f
Pardim93 <wellingtons_10@hotmail.com>
2019-01-23 19:32:11 -0800

    add functionality

--- rnd_addr.py
+++ rnd_addr.py
@@ -1,9 +1,14 @@
 import random, socket, struct, threading
-import requests, bs4, logging
-
+import requests, bs4, logging, argparse
 # Globals
 found_site_event = threading.Event()
 sites = []
+
+def write_found_sites():
+    with open('found_sites.txt', 'w') as file:
+        for site in sites:
+            file.write("%s\n" % site)
+
 def search_addr():
     while not found_site_event.is_set():
         addr = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
@@ -14,23 +19,38 @@ def search_addr():
             html = bs4.BeautifulSoup(r.text,features="html5lib")
             logging.debug(html.title.text +' - '+ addr)
             sites.append(addr)
+            print(sites)
             found_site_event.set()
         except:
             logging.debug('IP: ' +addr + ' - Not reachable.')
 
-def main():
+
+def set_search(thread_args, ip_args):
     threads = []
     logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s',
                     )
 
-    for i in range(4):
+    for i in range(thread_args):
         t = threading.Thread(target=search_addr)
         threads.append(t)
         t.start()
 
     logging.debug(sites)
     # search_addr(headers)
+
+def main():
+    # config parser
+    parser = argparse.ArgumentParser(description='Get random accessible IP addresses (Port 80).')
+    parser.add_argument('--threads',  default=1, type=int, help='Amount of threads (default = 1)')
+    parser.add_argument('--ip' , default=1, type=int, help='Amount of IP to find (default = 1)')
+
+    args = parser.parse_args()
+    thread_args, ip_args =  args.ip, args.threads
+    set_search(thread_args, ip_args)
+
+    found_site_event.wait()
+    write_found_sites()
 
 if __name__ == "__main__":
     main()
